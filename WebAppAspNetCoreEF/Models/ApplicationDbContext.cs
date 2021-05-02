85c1436d0cf87dab6e8d96f27e528ac0d9877081
DaniloMV <danilo7valladarez@gmail.com>
2021-05-01 22:06:45 -0500

    Iniciando la configuración DbContext

---
+++ WebAppAspNetCoreEF/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppAspNetCoreEF.Models
+{
+    /// <summary>
+    /// Dar ctrl + (.) Punto seleccionar Generar options esto es lo que hace es que me permite crear un,
+    /// constructor a través del cual yo voy a recibir las configuraciones realizadas a Entity Framework.
+    /// Esas configuraciones son por ejemplo que vamos a utilizar SQL Server que vamos a utilizar logs, etc.
+    /// Vamos a traer este namespace (NotNullAttribute) que indica qué opciones no puede ser nulo.
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
